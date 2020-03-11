d7f2b172873d8814d10285acb1939b22a2a8002b
Uros <uros.drobnjak@outlook.com>
2020-03-11 03:03:35 +0100

    Finished the project

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header(){
+    return (
+        <header>
+            <img src="https://i.pinimg.com/originals/9c/c2/f7/9cc2f7b8f0218b5df3d0d73deddb5747.jpg" alt="Problem?" />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
